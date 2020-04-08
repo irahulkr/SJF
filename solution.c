1def996c2793f16caf410b4404b180f4763507e2
irahulkr <44754497+irahulkr@users.noreply.github.com>
2020-04-09 02:41:44 +0530

    Update solution.c

--- solution.c
+++ solution.c
@@ -1,18 +1,20 @@
-//In this we are taking the input from terminal and not from any file
-
-
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-  int p[20], burst_time[20], waiting_time[20], turn_around_time[20], i, k, n=5, temp;
+  int p[20], burst_time[20], waiting_time[20], turn_around_time[20], i=0, k, n=5, temp;
   float waiting_time_avg, turn_around_time_avg;
-  for(i=0;i<n;i++)
-  {
-    p[i]=i;
-    printf("Enter Burst Time for Process %d -- ", i);
-    scanf("%d", &burst_time[i]);
-  }
+  FILE *fp = fopen("cpu_burst.txt", "r");
+  while((getc(fp))!=EOF)
+    {
+
+           fscanf(fp, "%d", &burst_time[i]);
+           if(burst_time[i]>0)
+            {
+               p[i]=i+1;
+                i++;
+            }
+    }
   for(i=0;i<n;i++)
   {
     for(k=i+1;k<n;k++)
@@ -44,4 +46,3 @@ int main()
   printf("\nAverage Turnaround Time -- %f", turn_around_time_avg/n);
   getch();
 }
-
